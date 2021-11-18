42a6b667246520bb3408559f19c61587f10fa03d
glister <PotatoSaladMaster>
2021-11-18 14:15:02 +0700

    Lesson_2

---
+++ lesson_2/numeric_operators.py
@@ -0,0 +1,9 @@
+x,y = 6,7
+print('Operands:', x,y)
+print('\nSubstraction:', x-y)
+print('\nAddition:', x+y)
+print('\nMultiplication:', x*y)
+print('\nDivision:', x/y)
+print('\nPower:', x**y)
+print('\nReminder:', y%x)
+print('\nSqrt:', x**0.5)
---
+++ lesson_2/types.py
@@ -0,0 +1,5 @@
+x = 1+1
+y = 'Surprize' + 'Motherfucker'
+print(type(x))
+print(type(y))
+print(type(66.6))
